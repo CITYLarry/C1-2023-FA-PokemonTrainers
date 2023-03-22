5d95400c1a3479c1c684dbbcb0d239cb5c17b16d
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-03-22 09:51:17 -0500

    initial commit

---
+++ src/test/java/co/com/sofka/pokemontrainers/PokemonTrainersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.pokemontrainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTrainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

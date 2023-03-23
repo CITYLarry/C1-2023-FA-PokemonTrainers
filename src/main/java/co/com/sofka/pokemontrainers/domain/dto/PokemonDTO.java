04d518266e036148c8cb7904984552382a2b9a56
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-03-23 16:04:02 -0500

    Replace Pokemon for PokemonDTO

--- src/main/java/co/com/sofka/pokemontrainers/domain/dto/PokemonDTO.java
+++ src/main/java/co/com/sofka/pokemontrainers/domain/dto/PokemonDTO.java
@@ -1,13 +1,16 @@
-package co.com.sofka.pokemontrainers.domain.collection;
+package co.com.sofka.pokemontrainers.domain.dto;
+
+import lombok.Data;
 
 import java.util.List;
 
-public class Pokemon {
+@Data
+public class PokemonDTO {
 
     private String pkmnId;
     private String pkdxNumber;
     private String name;
     private String nickname;
     private List<String> typeList;
-    private Boolean inTeam = false;
+    private Boolean inTeam;
 }
